d9b5bb15ca9d9901b7bb88c029391d26c03d2777
ademolaalashe <77229886+ademolaalashe@users.noreply.github.com>
2023-01-22 23:25:26 +0000

    Add JS questions in questions.js

---
+++ assets/js/logic.js
@@ -0,0 +1,4 @@
+let score = 0;
+const timeLimit = 60; // time in seconds
+let timeLimit = timeLimit;
+
---
+++ assets/js/questions.js
@@ -0,0 +1,33 @@
+const codeQuiz = [
+    {
+        question: "Javascript is an _______ language?"
+        options: ["Object-Oriented", "Object-Based", "Procedural", "None of the above"]
+        answer: "Object-Oriented"
+    }
+
+    {
+        question: "Which of the following keywords is used to define a variable in Javascript?"
+        options: ["var", "let", "Both A and B", "None of the above"]
+        answer: "Both A and B"
+    }
+
+    {
+        question: "Both var and let keywords are used to define a variable in Javascript?"
+        options: ["getElementbyId()", "getElementsByClassNAme()", "Both A and B", "None of the above"]
+        answer: "Both A and B"
+    }
+
+    {
+        question: "Upon encountering empty statements, what does the Javascript Interpreter do?"
+        options: ["Throws and error", "Ignores the statements", "Gives a warning", "None of the above"]
+        answer: "Ignores the statements"
+    }
+
+    {
+        question: "Which of the following methods can be used to display data in some form using Javascript?"
+        options: ["document.write()", "console.log()", "windows.alert()", "All of the above"]
+        answer: "All of the above"
+    }
+];
+
+
