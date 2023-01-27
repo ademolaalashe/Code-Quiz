610adccaae07d61f0dee69eabbd5f599d3c123b9
ademolaalashe <77229886+ademolaalashe@users.noreply.github.com>
2023-01-27 13:33:20 +0000

    Create button, add code to get value of button

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,100 +1,58 @@
-
 // Here, we are gettiing the HTML element
 
-const timeDisplay = document.getElementById('time');
-const startScreen = document.getElementById('start-screen');
-const startButton = document.getElementById('start');
-const questionsBox = document.getElementById('questions');
-const questionTitle = document.getElementById('question-title');
-const choices = document.getElementById('choices');
-const endScreen = document.getElementById('end-screen');
-const finalScore = document.getElementById('final-score');
-const initials = document.getElementById('initials');
-const submit = document.getElementById('submit');
-const feedback = document.getElementById('feedback');
+const timeDisplay = document.getElementById("time");
+const startScreen = document.getElementById("start-screen");
+const startButton = document.getElementById("start");
+const questionsBox = document.getElementById("questions");
+const questionTitle = document.getElementById("question-title");
+const choices = document.getElementById("choices");
+const endScreen = document.getElementById("end-screen");
+const finalScore = document.getElementById("final-score");
+const initials = document.getElementById("initials");
+const submit = document.getElementById("submit");
+const feedback = document.getElementById("feedback");
 
 // We are adding event listener to innitialize the start function when the button in clicked
-startButton.addEventListener('click', start);
+
+startButton.addEventListener("click", start);
+// start();
 function start() {
-    startScreen.classList.add("hide");
-    questionsBox.classList.remove("hide");
-    // On the start, start the timer
+  startScreen.classList.add("hide");
+  questionsBox.classList.remove("hide");
+  // On the start, start the timer
 }
 
 // How to access the value of the key inside the object
-console.log(codeQuiz[0].options)
-console.log(codeQuiz[0].question)
+console.log(codeQuiz[0].options);
+console.log(codeQuiz[0].question);
 
 //telling to set the text inside h2 to the value I want
 questionTitle.innerText = codeQuiz[0].question;
 
-let answers = codeQuiz[0].options
-let answerButton;
+let answers = codeQuiz[0].options;
 for (let i = 0; i < answers.length; i++) {
-//create button inside
+    let answerButton;
+  //create button inside
 
-answerButton = document.createElement("button");
-choices.appendChild(answerButton);
-answerButton.setAttribute("value", answers[i]);
-answerButton.innerText = answers[i];
+  answerButton = document.createElement("button");
+  choices.appendChild(answerButton);
+  answerButton.setAttribute("value", answers[i]);
+  answerButton.innerText = answers[i];
+  answerButton.addEventListener("click", checkAnswer);
 }
 
 // Function for checking the correct answer
-answerButton.addEventListener('click', checkAnswer);
-function checkAnswer(event){
+function checkAnswer(event) {
+    console.log('i was clicked')
+  //get the value of the button
+  let selectedAnswer = event.target.value;
 
-//get the value of the button
-let selectedAnswer = event.target.value
-
-if (selectedAnswer === codeQuiz[0].answer) {
+  if (selectedAnswer === codeQuiz[0].answer) {
     console.log("correct answer");
+  }
 }
 
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let score = 0;
-// const timeLimit = 60; // time in seconds
-// let timeLimit = timeLimit;
-
-// const intervalId = setInterval(() => {
-//     timeLeft--;
-//     console.log(timeLeft + " seconds remaining");
-//     if (timeLeft === 0) {
-//         clearInterval(intervalId);
-//         alert("Time's up! Your final score is " + score + " out of " + questions.length);       
-//     }
-// }, 1000);
-
-// for (let i = 0; i <questions.length; i++) {
-//     if (timeLeft === 0) {
-//         break;
-//     }
-//     const currentQuestion = questions[i];
-//     const userAnswer = prompt(currentQuestion.question + " " + currentQuestion.options.join(", "));
-//     if (userAnswer === currentQuestion.answer) {
-//         score++;
-//     }
-// }
-
-// if (timeLeft > 0) {
-//     clearInterval(intervalId);
-//     alert("Congratulations, you have completed the quiz! Your final score is " + score + " out of " + question.length);
-// }
+// startScreenButton.addEventListener("click", start - screen);
+// timeDisplayButton.addEventListener("click", time);
+// submitButton.addEventListener("click", submit);
+// feedbackButton.addEventListener("click", feedback);
