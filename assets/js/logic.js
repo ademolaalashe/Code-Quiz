a1010def0d5c84344ed33d59ca271b27a8392944
ademolaalashe <77229886+ademolaalashe@users.noreply.github.com>
2023-02-23 23:34:15 +0000

    Add submit event listener and clean and edit code

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,5 +1,3 @@
-// Here, we are gettiing the HTML element
-
 const timeDisplay = document.getElementById("time");
 const startScreen = document.getElementById("start-screen");
 const startButton = document.getElementById("start");
@@ -15,68 +13,45 @@ const feedback = document.getElementById("feedback");
 function end() {
   questionsBox.classList.add("hide");
   endScreen.classList.remove("hide");
+  stopInterval();
 }
 
-// Quiz sound effect
-
-// let sfxRight = new Audio("starter/assets/sfx/correct.wav");
-// let sfxRight = new Audio("starter/assets/sfx/correct.wav");
-
 // Adding event listener to innitialize the start function when the button in clicked
-
 startButton.addEventListener("click", start);
-
 
 function start() {
   startScreen.classList.add("hide");
   questionsBox.classList.remove("hide");
   // On the start, start the timer
-  timeInterval()
+  timeInterval();
 }
 
 // Set timer for the quiz
-
 let count = 80;
 let timerInterval;
 
-let timeInterval = () => {
+function timeInterval() {
   timeDisplay.innerText = count;
 
-  timerInterval = setInterval(function(){
+  timerInterval = setInterval(function () {
     count--;
     timeDisplay.innerText = count;
 
-    if(count === 0) {
+    if (count === 0) {
       stopInterval();
       end();
     }
   }, 1000);
 }
 
-
-let stopInterval = function() {
-  // console.log("Time is up!");
-  clearInterval(timeInterval);
+function stopInterval() {
+  clearInterval(timerInterval);
 }
 
-timeDisplay.innerText = count
+timeDisplay.innerText = count;
 
 //Telling to set the text inside h2 to the value wanted
 questionTitle.innerText = codeQuiz[0].question;
-
-let answers = codeQuiz[0].options;
-for (let i = 0; i < answers.length; i++) {
-    let answerButton;
-  //Create button inside
-
-  answerButton = document.createElement("button");
-  choices.appendChild(answerButton);
-  answerButton.setAttribute("value", answers[i]);
-  answerButton.innerText = answers[i];
-  answerButton.addEventListener("click", checkAnswer);
-}
-
-// Function for checking the correct answer
 
 let currentQuestionIndex = 0;
 
@@ -113,9 +88,16 @@ function checkAnswer(event) {
   }
 }
 
+// Initialize the first question
+let answers = codeQuiz[0].options;
+for (let i = 0; i < answers.length; i++) {
+  let answerButton = document.createElement("button");
+  choices.appendChild(answerButton);
+  answerButton.setAttribute("value", answers[i]);
+  answerButton.innerText = answers[i];
+  answerButton.addEventListener("click", checkAnswer);
+}
 
-
-// startScreenButton.addEventListener("click", start - screen);
-// timeDisplayButton.addEventListener("click", time);
-// submitButton.addEventListener("click", submit);
-// feedbackButton.addEventListener("click", feedback);
+submit.addEventListener('click', function(event) {
+  // code to execute when submit button is clicked
+});
