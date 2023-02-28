d096f279890f1955422203f60c5dc3640b1bc735
ademolaalashe <77229886+ademolaalashe@users.noreply.github.com>
2023-02-28 15:17:05 +0000

    Add local storage code and clean and update the entire code

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,27 +1,21 @@
-const timeDisplay = document.getElementById("time");
-const startScreen = document.getElementById("start-screen");
-const startButton = document.getElementById("start");
-const questionsBox = document.getElementById("questions");
-const questionTitle = document.getElementById("question-title");
-const choices = document.getElementById("choices");
-const endScreen = document.getElementById("end-screen");
-const finalScore = document.getElementById("final-score");
-const initials = document.getElementById("initials");
-const submit = document.getElementById("submit");
-const feedback = document.getElementById("feedback");
-
-function end() {
-  questionsBox.classList.add("hide");
-  endScreen.classList.remove("hide");
-  stopInterval();
-}
+const timeDisplay = document.getElementById('time');
+const startScreen = document.getElementById('start-screen');
+const startButton = document.getElementById('start');
+const questionsBox = document.getElementById('questions');
+const questionTitle = document.getElementById('question-title');
+const choices = document.getElementById('choices');
+const endScreen = document.getElementById('end-screen');
+const finalScore = document.getElementById('final-score');
+const initialsInput = document.getElementById('initials');
+const submit = document.getElementById('submit');
+const feedback = document.getElementById('feedback');
 
 // Adding event listener to innitialize the start function when the button in clicked
-startButton.addEventListener("click", start);
+startButton.addEventListener('click', start);
 
 function start() {
-  startScreen.classList.add("hide");
-  questionsBox.classList.remove("hide");
+  startScreen.classList.add('hide');
+  questionsBox.classList.remove('hide');
   // On the start, start the timer
   timeInterval();
 }
@@ -44,18 +38,9 @@ function timeInterval() {
   }, 1000);
 }
 
-function stopInterval() {
-  clearInterval(timerInterval);
-}
-
-timeDisplay.innerText = count;
-
 //Telling to set the text inside h2 to the value wanted
 questionTitle.innerText = codeQuiz[0].question;
-
 let currentQuestionIndex = 0;
-
-
 let score = 0;
 
 // variable to keep track of whether a question has been answered
@@ -66,12 +51,13 @@ function checkAnswer(event) {
   let selectedAnswer = event.target.value;
 
   if (selectedAnswer === codeQuiz[currentQuestionIndex].answer) {
-    feedback.innerText = "Correct!";
-    score +=1
+    feedback.innerText = 'Correct!';
+    score += 1;
     // This code is to display if the selected answer is correct
-    feedback.classList.remove("hide");
+    feedback.classList.remove('hide');
   } else {
-    feedback.innerText = "Wrong. The correct answer is: " + codeQuiz[currentQuestionIndex].answer;
+    feedback.innerText =
+      'Wrong. The correct answer is: ' + codeQuiz[currentQuestionIndex].answer;
     count -= 10; // reduce timer by 10 seconds
     // Code to display is answer is wrong
     feedback.classList.remove('hide');
@@ -82,39 +68,56 @@ function checkAnswer(event) {
   // Code to move to the next question
   currentQuestionIndex++;
   if (currentQuestionIndex >= codeQuiz.length) {
-  // End the quiz if we've reached the end of the questions
+    // End the quiz if we've reached the end of the questions
     end();
   } else {
-     // Code to update the UI to display the next question
-     questionTitle.innerText = codeQuiz[currentQuestionIndex].question;
-     choices.innerHTML = ""; // clear previous choices
-     let answers = codeQuiz[currentQuestionIndex].options;
-     for (let i = 0; i < answers.length; i++) {
-       let answerButton = document.createElement("button");
-       choices.appendChild(answerButton);
-       answerButton.setAttribute("value", answers[i]);
-       answerButton.innerText = answers[i];
-       answerButton.addEventListener("click", checkAnswer);
-     }
+    // Code to update the UI to display the next question
+    questionTitle.innerText = codeQuiz[currentQuestionIndex].question;
+    choices.innerHTML = ''; // clear previous choices
+    let answers = codeQuiz[currentQuestionIndex].options;
+    for (let i = 0; i < answers.length; i++) {
+      let answerButton = document.createElement('button');
+      choices.appendChild(answerButton);
+      answerButton.setAttribute('value', answers[i]);
+      answerButton.innerText = answers[i];
+      answerButton.addEventListener('click', checkAnswer);
+    }
   }
-
 }
 
 // This code is to initialize the first question
 let answers = codeQuiz[0].options;
 for (let i = 0; i < answers.length; i++) {
-  let answerButton = document.createElement("button");
+  let answerButton = document.createElement('button');
   choices.appendChild(answerButton);
-  answerButton.setAttribute("value", answers[i]);
+  answerButton.setAttribute('value', answers[i]);
   answerButton.innerText = answers[i];
-  answerButton.addEventListener("click", checkAnswer);
+  answerButton.addEventListener('click', checkAnswer);
 }
 
-submit.addEventListener('click', function(event) {
-  // code to execute when submit button is clicked
-  if (!answered) {
-    score = 0; // if no question has been answered, set score to 0
-  }
+function end() {
+  questionsBox.classList.add('hide');
+  endScreen.classList.remove('hide');
+  clearInterval(timerInterval);
+  feedback.classList.add('hide');
   finalScore.innerText = score;
-  location.href = "highscores.html";
-});
+  // initials submit button
+  submit.addEventListener('click', function (event) {
+    // prevent default submit button behaviour
+    event.preventDefault();
+
+    let highscores = JSON.parse(localStorage.getItem('highscores')) || [];
+    // save to localStorage
+    let initials = initialsInput.value.trim();
+    // format new score object for current user
+    let newScore = {
+      score: score,
+      initials: initials,
+    };
+
+    // save new score to localstorage
+    highscores.push(newScore);
+    localStorage.setItem('highscores', JSON.stringify(highscores));
+    location.href = 'highscores.html';
+  });
+}
